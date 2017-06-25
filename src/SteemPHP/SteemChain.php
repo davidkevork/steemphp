631c83f9220b4122f8246663226c3e4abcffe1a2
DavidKevork <davidkevork@hotmail.com>
2017-06-25 14:31:21 +1000

    SteemPHP\SteemChain contains 9 blochchain methods

---
+++ src/SteemPHP/SteemChain.php
@@ -0,0 +1,182 @@
+<?php
+
+namespace SteemPHP;
+
+use JsonRPC\Client;
+use JsonRPC\HttpClient;
+
+/**
+* SteemChain
+* 
+* This Class is contains only Chain Methods
+*/
+class SteemChain
+{
+
+	/**
+	 * @var $host
+	 * 
+	 * $host will be where our script will connect to fetch the data
+	 */
+	protected $host;
+
+	/**
+	 * @var $client
+	 * 
+	 * $client is part of JsonRPC which will be used to connect to the server
+	 */
+	protected $client;
+
+	/**
+	 * Initialize the connection to the host
+	 * @param String $host
+	 * 
+	 * $host = ['https://node.steem.ws', 'https://steemd.steemit.com']
+	 */
+	public function __construct($host = 'https://node.steem.ws')
+	{
+		$this->host = trim($host);
+		$this->httpClient = new HttpClient($this->host);
+		$this->httpClient->withoutSslVerification();
+		$this->client = new Client($this->host, false, $this->httpClient);
+	}
+
+	/**
+	 * Get Api number
+	 * @param String $name 
+	 * @return int
+	 */
+	public function getApi($name)
+	{
+		return $this->client->call(1, 'get_api_by_name', [$name]);
+	}
+
+	/**
+	 * Get Current blockchain version with steem and fc revision
+	 * @return array
+	 */
+	public function getVersion()
+	{
+		try {
+			$this->api = $this->getApi('login_api');
+			return $this->client->call($this->api, 'get_version', []);
+		} catch (Exception $e) {
+			return  $e->getMessage();
+		}
+	}
+
+	/**
+	 * Get the total number of registered steem accounts
+	 * @return int
+	 */
+	public function getAccountCount()
+	{
+		try {
+			$this->api = $this->getApi('database_api');
+			return $this->client->call($this->api, 'get_account_count', []);
+		} catch (Exception $e) {
+			return $e->getMessage();
+		}
+	}
+
+	/**
+	 * Get currency blockchain properties
+	 * @return array
+	 */
+	public function getChainProperties()
+	{
+		try {
+			$this->api = $this->getApi('database_api');
+			return $this->client->call($this->api, 'get_chain_properties', []);
+		} catch (Exception $e) {
+			return $e->getMessage();
+		}
+	}
+
+	/**
+	 * Get blockchain configuration
+	 * @return array
+	 */
+	public function getConfig()
+	{
+		try {
+			$this->api = $this->getApi('database_api');
+			return $this->client->call($this->api, 'get_config', []);
+		} catch (Exception $e) {
+			return $e->getMessage();
+		}
+	}
+
+	/**
+	 * Get Dynamic Global Properties
+	 * @return array
+	 */
+	public function getDynamicGlobalProperties()
+	{
+		try {
+			$this->api = $this->getApi('database_api');
+			return $this->client->call($this->api, 'get_dynamic_global_properties', []);
+		} catch (Exception $e) {
+			return $e->getMessage();
+		}
+	}
+
+	/**
+	 * Get Feed History
+	 * @return array
+	 */
+	public function getFeedHistory()
+	{
+		try {
+			$this->api = $this->getApi('database_api');
+			return $this->client->call($this->api, 'get_feed_history', []);
+		} catch (Exception $e) {
+			return $e->getMessage();
+		}
+	}
+
+	/**
+	 * Get Current Median History Price
+	 * @return array
+	 */
+	public function getCurrentMeidanHistoryPrice()
+	{
+		try {
+			$this->api = $this->getApi('database_api');
+			return $this->client->call($this->api, 'get_current_median_history_price', []);
+		} catch (Exception $e) {
+			return $e->getMessage();
+		}
+	}
+
+	/**
+	 * Get current Hardfork version
+	 * @return version
+	 */
+	public function getHardforkVersion()
+	{
+		try {
+			$this->api = $this->getApi('database_api');
+			return $this->client->call($this->api, 'get_hardfork_version', []);
+		} catch (Exception $e) {
+			return $e->getMessage();
+		}
+	}
+
+	/**
+	 * Get next scheduled hardfork version and date
+	 * @return array
+	 */
+	public function getNextScheduledHardfork()
+	{
+		try {
+			$this->api = $this->getApi('database_api');
+			return $this->client->call($this->api, 'get_next_scheduled_hardfork', []);
+		} catch (Exception $e) {
+			return $e->getMessage();
+		}
+	}
+
+}
+
+?>
