14e946e79da838bf65ec65ab792d32d1e3ef6d93
DavidKevork <davidkevork@hotmail.com>
2017-06-25 16:20:17 +1000

    SteemChain PHP unit test

---
+++ tests/SteemChainTest.php
@@ -0,0 +1,63 @@
+<?php
+
+include (__DIR__).'/../vendor/autoload.php';
+
+class SteemChainTest extends PHPUnit_Framework_TestCase
+{
+	protected function setUp()
+	{
+		$this->SteemChain = new \SteemPHP\SteemChain('https://node.steem.ws');
+	}
+
+	public function testGetApi()
+	{
+		$this->assertInternalType('int', $this->SteemChain->getApi('login_api'));
+	}
+
+	public function testGetVersion()
+	{
+		$this->assertArrayHasKey('blockchain_version', $this->SteemChain->getVersion());
+	}
+
+	public function testGetAccountCount()
+	{
+		$this->assertInternalType('int', $this->SteemChain->getAccountCount());
+	}
+
+	public function testGetChainProperties()
+	{
+		$this->assertArrayHasKey('account_creation_fee', $this->SteemChain->getChainProperties());
+	}
+
+	public function testGetConfig()
+	{
+		$this->assertArrayHasKey('SBD_SYMBOL', $this->SteemChain->getConfig());
+	}
+
+	public function testGetDynamicGlobalProperties()
+	{
+		$this->assertArrayHasKey('head_block_id', $this->SteemChain->getDynamicGlobalProperties());
+	}
+
+	public function testGetFeedHistory()
+	{
+		$this->assertArrayHasKey('id', $this->SteemChain->getFeedHistory());
+	}
+
+	public function testGetCurrentMeidanHistoryPrice()
+	{
+		$this->assertArrayHasKey('base', $this->SteemChain->getCurrentMeidanHistoryPrice());
+	}
+
+	public function testGetHardforkVersion()
+	{
+		$this->assertInternalType('string', $this->SteemChain->getHardforkVersion());
+	}
+
+	public function testGetNextScheduledHardfork()
+	{
+		$this->assertArrayHasKey('hf_version', $this->SteemChain->getNextScheduledHardfork());
+	}
+}
+
+?>
