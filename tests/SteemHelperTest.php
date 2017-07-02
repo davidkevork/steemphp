b40d4f83686fce5278e0ca7c7af4f3f148e0f59b
DavidKevork <davidkevork@hotmail.com>
2017-07-02 17:14:25 +1000

    SteemHelperTest new method added

--- tests/SteemHelperTest.php
+++ tests/SteemHelperTest.php
@@ -12,7 +12,7 @@ class SteemHelperTest extends PHPUnit_Framework_TestCase
 		$this->assertInternalType('string', SteemHelper::toInt('500 STEEM'));
 	}
 
-	public function test_filter_int()
+	public function testFilterInt()
 	{
 		$this->assertInternalType('int', SteemHelper::filterInt(500));
 	}
@@ -22,9 +22,19 @@ class SteemHelperTest extends PHPUnit_Framework_TestCase
 		$this->assertInternalType('int', SteemHelper::reputation(3000000));
 	}
 
-	public function vestToSteem()
+	public function testFilterDate()
+	{
+		$this->assertEquals('2017-06-29T22-00-00', SteemHelper::filterDate('2017-06-30'));
+	}
+
+	public function testVestToSteem()
 	{
 		$this->assertInternalType('float', SteemHelper::vestToSteem('24477.640182 VESTS', '369735200112.175967 VESTS', '178661603.552 STEEM'));
+	}
+
+	public function testContains()
+	{
+		$this->assertEquals('1', SteemHelper::contains('david', 'davidk'));
 	}
 
 	public function testCharAt()
