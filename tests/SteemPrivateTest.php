61c31828676cbf849c1a6fe4ddc001f7c6ff2bea
DavidKevork <davidkevork@hotmail.com>
2017-09-01 19:23:34 +1000

    fixed error in test file

--- tests/SteemPrivateTest.php
+++ tests/SteemPrivateTest.php
@@ -22,12 +22,12 @@ class SteemPrivateTest extends PHPUnit_Framework_TestCase
 
 	public function testFromSeed()
 	{
-		$this->assertEquals('5JpHaesrxhiaBsqNxWFAWkkykVBPTqs1KdiRLhcBbpLjiVDRypg', $this->SteemPrivate->fromSeed('guest1235JRaypasxMx1L97ZUX7YuC5Psb5EAbF821kkAGtBj7xCJFQcbLgowner'));
+		$this->assertEquals('5JN8BCwLUw3CddU9tGjyWX7ahV9iye1tfsDzn9Ab5tg6kzd1fZ8', $this->SteemPrivate->fromSeed('guest1235JRaypasxMx1L97ZUX7YuC5Psb5EAbF821kkAGtBj7xCJFQcbLgowner'));
 	}
 
 	public function testFromBuffer()
 	{
-		$this->assertEquals('5JpHaesrxhiaBsqNxWFAWkkykVBPTqs1KdiRLhcBbpLjiVDRypg', $this->SteemPrivate->fromBuffer('484aaef3bc4f8983a6ae44526f39f949b7cdd731db7d6f87a93b1432b3dde468'));
+		$this->assertEquals('5JN8BCwLUw3CddU9tGjyWX7ahV9iye1tfsDzn9Ab5tg6kzd1fZ8', $this->SteemPrivate->fromBuffer('484aaef3bc4f8983a6ae44526f39f949b7cdd731db7d6f87a93b1432b3dde468'));
 	}
 
 	public function testIsWif()
